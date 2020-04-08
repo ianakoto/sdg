02baab7554c84bc893b25805344e7dad88eb0a37
ian akoto <iancecilakoto@gmail.com>
2020-04-08 03:26:35 -1200

    initializing project

--- src/estimator.py
+++ src/estimator.py
@@ -1,2 +1,25 @@
 def estimator(data):
-  return data
+      """ THis function computes the estimation of covid 19 infected patients
+      
+      input: data as a dict
+      
+      data : 1. region as a dict
+                            region: i. name as string
+                                    ii. avgAge as double
+                                    iii. avgDailyIncomeInUSD as double
+                                    iv. avgDailyIncomePopulation as double
+
+             2. periodType as string
+             3. timeToElapse as an int
+             4. reportedCases as an int
+             5. population as an int
+             6. totalHospitalBeds: as an int
+
+       returns   a dictionary of input data, impact as dict, and severImpact as dict    
+      
+       """
+
+       
+      return data
+      
+       
