ba4288c3e545c04ae54736c8830116f9d79df378
ian akoto <iancecilakoto@gmail.com>
2020-04-13 15:13:46 -1200

    deco1

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -119,11 +119,11 @@ class Get_Logging(Resource):
         mlogs = Logs.query.all()
         put_data = []
         for log in mlogs:
-            put_data.append(f"{log.httpmethod}          {log.requestpath}               {log.status}          {log.timetook} ms  ")
+            put_data.append(f'{log.httpmethod}     \t\t    {log.requestpath}       \t\t         {log.status}      \t\t     {log.timetook} ms \n ')
 
 
 
-        return  put_data, 200, {'Content-Type': 'text/plain; charset=utf-8'}        
+        return ' '.join(put_data), 200, {'Content-Type': 'text/plain; charset=utf-8'}        
 
 
 
