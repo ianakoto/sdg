3b403ed4be3c7f24ce214563006a3b7feb254824
Ian Cecil Mawuli Akoto <iannakoto@yahoo.com>
2020-04-14 22:31:01 -1200

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -5,8 +5,8 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'password'
-    SQLALCHEMY_DATABASE_URI = "postgres://dpiwjukxirskcf:391dae50ebad903e6e16e6d303c44e5181785cd7ac9bc23e8caa5587f9e46dc1@ec2-54-210-128-153.compute-1.amazonaws.com:5432/d19mg8ans3m4qk"
+    SECRET_KEY = ''
+    SQLALCHEMY_DATABASE_URI = ""
 
 
 class ProductionConfig(Config):
